2d287247741e39800933c7bfa6d68576960118af
jefferson.sales <jefferson.sales@fretebras.com.br>
2023-02-07 22:26:49 +0000

    feat: added function to domain events

--- src/Core/Domain/AggregateRoot.php
+++ src/Core/Domain/AggregateRoot.php
@@ -1,6 +1,25 @@
 <?php
 namespace Module\Core\Domain;
+
+use Module\Core\Domain\Contracts\DomainEvent;
+
 abstract class AggregateRoot extends Entity
 {
+    private array $domainEvents;
+
+    protected function addEvent(/* DomainEvent */ $event)
+    {
+        $this->domainEvents[] = $event;
+    }
+
+    public function clearEvents(): void
+    {
+        $this->domainEvents = [];
+    }
+
+    public function getEvents(): array
+    {
+        return $this->domainEvents;
+    }
 
 }
