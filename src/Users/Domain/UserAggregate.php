8d564abdb72186ca7ca1a126ea46ec92b180775b
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-03-06 23:56:29 -0300

    refactor: remove unnecessary implementation

--- src/Users/Domain/UserAggregate.php
+++ src/Users/Domain/UserAggregate.php
@@ -6,9 +6,8 @@ namespace Module\Users\Domain;
 
 use Module\Core\Domain\AggregateRoot;
 use Module\Core\Domain\Errors\DomainError;
-use Module\Core\Infrastructure\Ecotone\Contracts\ISerializeToQueue;
 
-class UserAggregate extends AggregateRoot implements ISerializeToQueue
+class UserAggregate extends AggregateRoot
 {
     public function __construct(
         ?string $uuid,
@@ -29,44 +28,6 @@ class UserAggregate extends AggregateRoot implements ISerializeToQueue
         $instance->addEvent(['deu_bom' => 'huehuebrbr']);
 
         return $instance;
-    }
-
-    public function getEmail(): Email
-    {
-        return $this->email;
-    }
-
-    public function getPassword(): Password
-    {
-        return $this->password;
-    }
-
-    public function toArray(): array
-    {
-        return [
-            'uuid' => $this->getUuid(),
-            'email' => $this->email->value,
-            'password' => $this->password->value,
-        ];
-    }
-
-    public static function fromArray(array $data): self
-    {
-        return new self(
-            $data['uuid'],
-            new Email($data['email']),
-            new Password($data['password'])
-        );
-    }
-
-    public function changeEmail(Email $newEmail)
-    {
-        $this->email = $newEmail;
-    }
-
-    public function changePassword(Password $password): void
-    {
-        $this->password = $password;
     }
 
     private static function validate($uuid = null, $email = null): \ArrayObject
